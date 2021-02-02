3d6f70df364015aab2ab80eba40a668bb40925be
Jacob Reckhard <jacobreckhard@gmail.com>
2021-02-02 16:10:22 -0700

    webworker file loads, but unsuccessfully

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+type RustAPI = any;
+
 function App() {
+    const [rust, setRust] = useState<RustAPI | null>(null);
+    useEffect(() => {
+        const emulatorworker = new Worker(process.env.PUBLIC_URL + '/emulatorworker.js')
+        emulatorworker.onmessage = (m) => console.log(m.data);
+    });
+    return (rust !== null) ? <Main rust={rust}/> : <p>Loading...</p>;
+}
+
+function Main(props: {rust: RustAPI}) {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+      const go = async () => {
+        console.log(props);
+        //const update = () => setCount(props.rust.check_x());
+        //setTimeout(update, 300);
+      };
+      go();
+  });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> Hello World </p>
+        <p> Rust count is at: {count} </p>
       </header>
     </div>
   );
