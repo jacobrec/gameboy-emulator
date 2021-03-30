6079b618a6e4d04882d2aef11ef870e5002406ac
Izu Eneh <ehen@ualberta.ca>
2021-03-30 16:57:09 -0600

    added dashboard

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,61 +1,185 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Emulator from './Emulator';
 import { useForm } from 'react-hook-form';
-import Modal from 'react-modal';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import Hidden from '@material-ui/core/Hidden';
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import SaveIcon from '@material-ui/icons/Save';
+import SettingsIcon from '@material-ui/icons/Settings';
+import GetAppIcon from '@material-ui/icons/GetApp';
 
-function ButtonAppBar(props: any) {
-  const classes = useStyles();
+const drawerWidth = 240;
 
-  return (
-    <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            {props.name}
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
-  );
-}
+const useStyles2 = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+    },
+    drawer: {
+      [theme.breakpoints.up('sm')]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+      [theme.breakpoints.up('sm')]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+      },
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up('sm')]: {
+        display: 'none',
+      },
+    },
+    // necessary for content to be below app bar
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing(3),
+    },
+  }),
+);
 
 function FileSubmission(props: any) {
   const { register, handleSubmit, errors } = useForm(); 
 
   return (
-    <Modal
-      isOpen={props.isOpen}
-    >
-      <h2>Load a Game</h2>
-      <form onSubmit={handleSubmit(props.onSubmit)} className="form">
-        <input required type="file" name="rom" ref={register}/>
-        <button>Submit</button>
-      </form>
-    </Modal>
+    <div className="modal-box">
+      <div className="modal-text">
+        <h2>Load a Game</h2>
+        <form onSubmit={handleSubmit(props.onSubmit)} className="form">
+          <input required type="file" name="rom" ref={register}/>
+          <button>Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+function GamePad() {
+  return (
+    <div className="gamepad">
+      GamePad
+    </div>
+  );
+}
+
+function Screen() {
+  return (
+    <div className="screen">
+      Screen
+    </div>
+  );
+}
+
+function ResponsiveDrawer(props: any) {
+  const classes = useStyles2();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const swtch = (index: number) => {
+    switch(index) {
+      case 0: return <GetAppIcon/>;
+      case 1: return <SaveIcon/>;
+      case 2: return <SettingsIcon/>;
+
+      default: return <h1>No project match</h1>
+    }
+  };
+
+  const drawerContent = (
+    <div>
+      <div className={classes.toolbar} />
+      <Divider />
+      <List>
+        {['Load', 'Save', 'Settings'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>
+              {swtch(index)}
+            </ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            className={classes.menuButton}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            {props.name}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden smUp implementation="css">
+          <Drawer
+            variant="temporary"
+            anchor='left'
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawerContent}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawerContent}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+      </main>
+    </div>
   );
 }
 
@@ -89,11 +213,23 @@ function App() {
 
   return (
     <div className="App">
-      <ButtonAppBar name={rom.name}/>
-      <FileSubmission onSubmit={onSubmit} isOpen={modalIsOpen}/>
-      <div>{/* Display screen */}</div>
-      <div>{/* modal for settings/other options possibly modal on modile and dashboard otherwise*/}</div>
-      <div>{/* controls */}</div>
+      <Modal open={modalIsOpen}>
+        <FileSubmission onSubmit={onSubmit}/>
+      </Modal>
+      <ResponsiveDrawer name={rom.name}/>
+      <Grid
+        direction="column"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item>
+          <Screen/>
+        </Grid>
+        <Divider/>
+        <Grid item>
+          <GamePad/>
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
